5abe35db58e27b045e565e125189575147db28a8
alu98@naver.com <alu98@naver.com>
2024-03-26 09:07:04 +0900

    commit

---
+++ 06_slot/src/components/04_dynamicslot/DynamicParent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="parentContainer">
+        <h1>DynamicSlot: </h1>
+        <input type="text" v-model="dynamicName">
+        <DynamicChild>
+            <template #[dynamicName]>
+                {{ dynamicName }}
+            </template>
+        </DynamicChild>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import DynamicChild from './DynamicChild.vue';
+
+    const dynamicName = ref('');
+</script>
+
+<style scoped>
+    .parentContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border: 1px solid black;
+    }
+</style>
