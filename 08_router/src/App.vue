ab41f8f499fdbe38f028e45c22e4b16afc015480
alu98@naver.com <alu98@naver.com>
2024-03-26 11:22:06 +0900

    commit

--- 08_router/src/App.vue
+++ 08_router/src/App.vue
@@ -1,8 +1,23 @@
 <script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+
 </script>
 
 <template>
-  
+  <header>
+    <h1>Router 사용하기</h1>
+    <hr>
+    <div class="wrapper">
+      <h2>basic router</h2>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <RouterView/>
+  </main>
 </template>
 
 <style scoped>
---
+++ 08_router/src/router/01_router.js
@@ -0,0 +1,11 @@
+import {createRouter, createWebHashHistory} from 'vue-router';
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            component: HomeView
+        }
+    ] 
+})
