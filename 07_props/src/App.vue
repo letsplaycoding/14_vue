fcc16cf4c07b1c2a8859599ae0b67c5ec9bdb220
alu98@naver.com <alu98@naver.com>
2024-03-26 10:25:53 +0900

    commit

--- 07_props/src/App.vue
+++ 07_props/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import PropsParents from './components/01_propsBasic/PropsParents.vue'
+  import DrillingParents from './components/02_propsDrilling/DrillingParents.vue'
   
 </script>
 
 <template>
   <header>
     <PropsParents/>
+    <DrillingParents/>
   </header>
 </template>
 
---
+++ 07_props/src/components/02_propsDrilling/DrillingParents.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script setup>
+    import { provide } from 'vue';
+
+    const count = ref(0);
+    provide('count', count);        // provide를 통해 별도의 공간에 count 반응형 변수값을 'count'라는 이름으로 담아둠
+</script>
+
+<style scoped>
+
+</style>
