aad110347bd70fec7345418be0e1a700221c3b27
alu98@naver.com <alu98@naver.com>
2024-03-26 09:19:04 +0900

    commit

--- 07_props/src/components/01_propsBasic/PropsParents.vue
+++ 07_props/src/components/01_propsBasic/PropsParents.vue
@@ -1,13 +1,24 @@
 <template>
     <div>
-
+        props 입력: <input v-model="message"/>
     </div>
 </template>
 
 <script setup>
-
+    import { ref } from 'vue'
+    const message = ref('Good Game.');
 </script>
 
 <style scoped>
+    .container{
+        border: 1px solid black;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
+    .dart {
+        background-color: black;
+        color: white;
+    }
 </style>
