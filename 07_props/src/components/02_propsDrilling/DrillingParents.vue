2a15d8ebc8e5a1daee33c4d5940eb024ad209282
alu98@naver.com <alu98@naver.com>
2024-03-26 10:44:23 +0900

    commit

--- 07_props/src/components/02_propsDrilling/DrillingParents.vue
+++ 07_props/src/components/02_propsDrilling/DrillingParents.vue
@@ -6,11 +6,13 @@
 </template>
 
 <script setup>
-    import { provide,ref } from 'vue';
+    import { provide,ref,readonly } from 'vue';
     import DrillingChild from './DrillingChild.vue';
 
     const count = ref(0);
-    provide('count', count);        // provide를 통해 별도의 공간에 count 반응형 변수값을 'count'라는 이름으로 담아둠
+    // provide('count', count);                  // provide를 통해 별도의 공간에 count 반응형 변수값을 'count'라는 이름으로 담아둠
+    provide('count', readonly(count));        // privide를 활용하는 부모 컴포넌트에서는 수정하지만 inject를 활용하는 곳은
+                                              // 읽기 전용으로 작성할 수 있다.(권장)
 </script>
 
 <style scoped>
