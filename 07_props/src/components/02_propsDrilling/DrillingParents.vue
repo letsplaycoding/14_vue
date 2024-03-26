b1a3a5140144135e77b7e98e71de87391c95ac29
alu98@naver.com <alu98@naver.com>
2024-03-26 10:27:58 +0900

    DrillingParents commit

--- 07_props/src/components/02_propsDrilling/DrillingParents.vue
+++ 07_props/src/components/02_propsDrilling/DrillingParents.vue
@@ -1,9 +1,12 @@
 <template>
-    
+    <div class="container">
+        <h1>Parents: {{ count }}</h1>
+        <Child/>
+    </div>
 </template>
 
 <script setup>
-    import { provide } from 'vue';
+    import { provide,ref } from 'vue';
 
     const count = ref(0);
     provide('count', count);        // provide를 통해 별도의 공간에 count 반응형 변수값을 'count'라는 이름으로 담아둠
